00b62b96d5077ab64ef4bd3fcb9f9363c9a4d730
hefeng6500 <2443992009@qq.com>
2024-01-27 21:51:48 +0800

    feat: 拦截器及异常

--- apps/low-code-test/src/app.controller.ts
+++ apps/low-code-test/src/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, VERSION_NEUTRAL, Version } from '@nestjs/common';
 import { AppService } from './app.service';
+import { BusinessException } from './common/exceptions/business.exception';
 
-@Controller()
+@Controller({
+  // path: 'user',
+  // version: '1',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  @Version([VERSION_NEUTRAL, '1'])
+  findAll() {
+    return this.appService.findAll();
+  }
+
+  @Get()
+  @Version('2')
+  findAll2() {
+    return 'i am new one';
+  }
+
+  @Get('findError')
+  @Version([VERSION_NEUTRAL, '1'])
+  findError() {
+    const a: any = {};
+    console.log(a.b.c);
+    return this.appService.getHello();
+  }
+
+  @Get('findBusinessError')
+  @Version([VERSION_NEUTRAL, '1'])
+  findBusinessError() {
+    const a: any = {};
+    try {
+      console.log(a.b.c);
+    } catch (error) {
+      throw new BusinessException('你这个参数错了');
+    }
+    return this.appService.findAll();
   }
 }
