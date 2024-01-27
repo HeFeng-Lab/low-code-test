59d7789b11d5411fda0a0b7ed7a9875a518afce5
hefeng6500 <2443992009@qq.com>
2024-01-27 22:14:48 +0800

    feat: 添加 swagger 文档

--- apps/low-code-test/src/main.ts
+++ apps/low-code-test/src/main.ts
@@ -4,9 +4,18 @@ import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { generateDocument } from './doc';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  // 异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
 
   // 接口版本化管理
   app.enableVersioning({
@@ -15,10 +24,14 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
 
-  app.useGlobalInterceptors(new TransformInterceptor());
+  // 创建文档
+  generateDocument(app);
 
-  // 异常过滤器
-  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+  // 添加热更新
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 
   await app.listen(3000);
 }
